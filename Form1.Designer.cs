d5240e58a6bfb15aba3b0e900d28e0dcddd4d134
webatxcent <web@xcent.com>
2020-11-04 14:58:09 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace POC_Spliiter {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing ) {
+            if ( disposing && ( components != null ) ) {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.parameterContainer1 = new POC_Spliiter.ParameterContainer();
+            this.SuspendLayout();
+            // 
+            // parameterContainer1
+            // 
+            this.parameterContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.parameterContainer1.Location = new System.Drawing.Point(18, 19);
+            this.parameterContainer1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.parameterContainer1.Name = "parameterContainer1";
+            this.parameterContainer1.Size = new System.Drawing.Size(1164, 688);
+            this.parameterContainer1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 727);
+            this.Controls.Add(this.parameterContainer1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ParameterContainer parameterContainer1;
+    }
+}
+
---
+++ ParameterContainer.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POC_Spliiter {
+    public partial class ParameterContainer : UserControl {
+
+        bool controlsCreated = false;
+
+        class TagData {
+            public Control LinkedControl { get; set; }
+            public int Position { get; set; }
+
+            public TagData( Control linkedControl, int position ) {
+                LinkedControl = linkedControl;
+                Position = position;
+            }
+        }
+
+
+        List<Label> labels = new List<Label>();
+        List<TextBox> textboxes = new List<TextBox>();
+
+        public ParameterContainer() {
+            InitializeComponent();
+        }
+
+        private Point _prevPan2Pos = new Point();
+
+        void PanelPaint( object sender, System.Windows.Forms.PaintEventArgs e ) {
+            SynchronizeSplitContainerScrollbars();
+        }
+
+        private void SynchronizeSplitContainerScrollbars() {
+            if ( splitContainer1.Panel2.AutoScrollPosition != _prevPan2Pos ) {
+                int margin = 4;
+                int maxheight = textboxes.Max( m => m.Height ) + ( margin * 2 );
+                foreach ( TextBox textbox in textboxes ) {
+                    TagData tagdata = textbox.Tag as TagData;
+                    Label label = tagdata.LinkedControl as Label;
+                    int position = tagdata.Position;
+                    label.Top = splitContainer1.Panel2.AutoScrollPosition.Y + position * ( maxheight + 1 ) + ( maxheight - label.Height ) / 2;
+                }
+
+
+                _prevPan2Pos = splitContainer1.Panel2.AutoScrollPosition;
+            }
+        }
+
+        protected override void OnCreateControl() {
+          
+            base.OnCreateControl();
+
+            splitContainer1.SplitterDistance = Width / 2;
+
+            splitContainer1.Panel2.Paint += PanelPaint;
+            splitContainer1.Panel2.Scroll += ( obj, scrollEventArgs ) => SynchronizeSplitContainerScrollbars();
+
+            splitContainer1.Panel1.AutoScroll = false;
+            splitContainer1.Panel2.AutoScroll = true;
+
+            splitContainer1.SplitterMoved += SplitContainer1_SplitterMoved;
+
+
+            for( int i = 0; i < 20; i++ ) {
+                Label label = new Label();
+                TextBox textbox = new TextBox();
+
+                label.Text = $"Label #{i}";
+                label.Tag = new TagData( textbox, i );
+                splitContainer1.Panel1.Controls.Add( label );
+                labels.Add( label );
+                label.Click += LabelClick;
+
+
+                textbox.Text = $"Textbox #{i}";
+                textbox.Tag = new TagData( label, i );
+                splitContainer1.Panel2.Controls.Add( textbox );
+                textboxes.Add( textbox );
+            }
+
+            controlsCreated = true;
+            LayoutControls();
+        }
+
+        private void LabelClick( object sender, EventArgs e ) {
+            Label label = sender as Label;
+            ( ( label.Tag as TagData ).LinkedControl as TextBox ).Focus();
+        }
+
+        private void SplitContainer1_SplitterMoved( object sender, SplitterEventArgs e ) {
+            int margin = 4;
+            int maxheight = textboxes.Max( m => m.Height ) + ( margin * 2 );
+            foreach ( TextBox textbox in textboxes ) {
+                TagData tagdata = textbox.Tag as TagData;
+                Label label = tagdata.LinkedControl as Label;
+                int position = tagdata.Position;
+
+                textbox.Width = splitContainer1.Panel2.Width - margin;
+
+                label.Left = margin;
+                label.Width = splitContainer1.Panel1.Width - margin;
+            }
+        }
+
+        void LayoutControls() {
+
+            int margin = 4;
+            int maxheight = textboxes.Max( m => m.Height ) + ( margin * 2 );
+            foreach ( TextBox textbox in textboxes ) {
+                TagData tagdata = textbox.Tag as TagData;
+                Label label = tagdata.LinkedControl as Label;
+                int position = tagdata.Position;
+
+                textbox.Top = position * ( maxheight + 1 ) + margin;
+                textbox.Left = 0;
+                textbox.Width = splitContainer1.Panel2.Width - margin;
+
+                label.Top = position * ( maxheight + 1 ) + ( maxheight - label.Height ) / 2;
+                label.Left = margin;
+                label.Width = splitContainer1.Panel1.Width - margin;
+                label.TextAlign = ContentAlignment.MiddleRight;
+
+           
+            }
+
+        }
+
+    }
+}
